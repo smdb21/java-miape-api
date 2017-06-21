f837936d10a635f3ee98f4a36b2c07da45ddd925
Salvador Martinez-Bartolome <salvador@scripps.edu>
2017-06-21 15:48:23 -0700

    thermo parser disabled compilation to 1.8

--- src/main/java/org/proteored/miapeapi/xml/msf/ThermoParser.java
+++ src/main/java/org/proteored/miapeapi/xml/msf/ThermoParser.java
@@ -1,24 +1,46 @@
 package org.proteored.miapeapi.xml.msf;
 
-import java.sql.SQLException;
-
-import com.compomics.thermo_msf_parser.Parser;
-
+/**
+ * In order to use this, include the library as:
+ * 
+ * 
+ * 
+ * <!-- Thermo MSF PARSER --> <dependency> <groupId>com.compomics</groupId>
+ * <artifactId>thermo_msf_parser</artifactId> <version>2.0</version>
+ * <exclusions> <exclusion> <groupId>batik</groupId>
+ * <artifactId>batik-all</artifactId> </exclusion> <exclusion>
+ * <artifactId>pdf-transcoder</artifactId> <groupId>batik</groupId> </exclusion>
+ * <exclusion> <groupId>com.compomics</groupId>
+ * <artifactId>utilities</artifactId> </exclusion> <exclusion>
+ * <groupId>itext</groupId> <artifactId>iText</artifactId> </exclusion>
+ * <exclusion> <groupId>jfree</groupId> <artifactId>jfreechart</artifactId>
+ * </exclusion> <exclusion> <groupId>jfree</groupId>
+ * <artifactId>jcommon</artifactId> </exclusion> <exclusion>
+ * <groupId>lennart</groupId> <artifactId>cpdetector</artifactId> </exclusion>
+ * <exclusion> <groupId>lennart</groupId> <artifactId>lmjunit</artifactId>
+ * </exclusion> <exclusion> <groupId>lennart</groupId>
+ * <artifactId>statlib</artifactId> </exclusion> <exclusion>
+ * <groupId>lennart</groupId> <artifactId>xxindex</artifactId> </exclusion>
+ * </exclusions> </dependency>
+ * 
+ * @author Salva
+ *
+ */
 public class ThermoParser {
 	public void main() {
 		String iMsfFileLocation = null;
 		boolean iLowMemory = false;
 		// ScoreType scoreType = new ScoreType();
-		try {
-			Parser msfParser = new Parser(iMsfFileLocation, iLowMemory);
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// try {
+		// // Parser msfParser = new Parser(iMsfFileLocation, iLowMemory);
+		//
+		// } catch (SQLException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// } catch (ClassNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 
 	}
 }
